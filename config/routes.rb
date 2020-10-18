e0fb43568afde13d4774ed3d0ffefeab2ce300cb
madking55 <madking55@gmail.com>
2020-10-18 16:47:54 +0200

    Finish User Story 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  get :root, to: 'welcome#index'
+
+  resources :merchants do
+    resources :items, only: [:index, :new, :create]
+  end
+
+  namespace :merchant do
+    get '/', to: 'dashboard#index', as: :dashboard
+  end
+
+  resources :items, only: [:index, :show, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:edit, :update, :destroy]
+
+  get '/cart', to: 'cart#show'
+  post '/cart/:item_id', to: 'cart#add_item'
+  delete '/cart', to: 'cart#empty'
+  patch '/cart/:change/:item_id', to: 'cart#update_quantity'
+  delete '/cart/:item_id', to: 'cart#remove_item'
+
+  resources :orders, only: [:new, :create, :show]
 
   get '/registration', to: 'users#new', as: :registration
   post '/users', to: 'users#create'
   get '/profile', to: 'users#show'
+
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#logout'
-
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
-
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
-
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
-
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
-
-  post "/cart/:item_id", to: "cart#add_item"
-  get "/cart", to: "cart#show"
-  delete "/cart", to: "cart#empty"
-  delete "/cart/:item_id", to: "cart#remove_item"
-
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
-  get "/orders/:id", to: "orders#show"
 end
